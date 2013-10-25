0b996226046306d5d22c63a3238eafcff1e92177
whhphd <whhphd@gmail.com>
2013-10-25 23:50:57 +0800

    Delete newjavascript.js

--- newjavascript.js
+++
@@ -1,8 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-x=document.getElementById("demo");  //查找元素
-x.innerHTML="Hello JavaScript";    //改变内容
